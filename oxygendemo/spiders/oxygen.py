25f5b6dc08a3351a0eba5dd11ef5f2f3d0e7e3cb
Damjan Košir <damjan.kosir@gmail.com>
2015-08-08 17:55:13 +1200

    function renames

--- oxygendemo/spiders/oxygen.py
+++ oxygendemo/spiders/oxygen.py
@@ -13,23 +13,26 @@ class OxygenSpider(CrawlSpider):
 
     def parse(self, response):
         # if we can't use it, we ended up somewhere strange
-        if self.canUseXPath(response):
+        if self.can_use_xpath(response):
             self.logger.info('checking: %s', response.url)
-            self.isProductPage(response)
+            if self.is_product_page(response):
+                yield self.parse_item(response)
 
             for href in response.xpath("//a/@href"):
                 url = response.urljoin(href.extract())
                 # don't go scraping javascript: links
+                # TODO filter images and other obviously wrong files
                 if url.startswith("http"):
                     yield scrapy.Request(url)
 
-    def isProductPage(self, response):
+    def is_product_page(self, response):
         if len(response.xpath('//div[@id="product-images"]')) > 0:
             self.logger.info('found product page: %s', response.url)
             return True
         return False
 
-    def canUseXPath(self, response):
+    @staticmethod
+    def can_use_xpath(response):
         return getattr(response, 'xpath', None) is not None
 
     def parse_item(self, response):
