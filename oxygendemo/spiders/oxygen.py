d0bd4a20b647c98fad40cafb2a5223b044cc36de
Damjan Košir <damjan.kosir@gmail.com>
2015-08-09 23:07:31 +1200

    adding missing item details

--- oxygendemo/spiders/oxygen.py
+++ oxygendemo/spiders/oxygen.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 import urllib
 from scrapy import Request
 from scrapy.contrib.spiders import CrawlSpider
@@ -42,8 +43,8 @@ class OxygenSpider(CrawlSpider):
             self.jewelry_urls_remaining.remove(url)
         if url in self.urls_before_designers_remaining:
             self.urls_before_designers_remaining.remove(url)
+
         # if we can't use it, we ended up somewhere strange
-        # TODO shouldn't be needed in the final version
         if self.can_use_xpath(response):
             self.logger.info('checking: %s', url)
 
@@ -59,13 +60,6 @@ class OxygenSpider(CrawlSpider):
                             self.jewelry_urls_remaining.add(new_url)
                         self.urls_before_designers_remaining.add(new_url)
 
-                        # # don't go scraping javascript: links
-                        # # TODO filter images and other obviously wrong files
-                        # if url.startswith("http") \
-                        #         and not url.startswith('http://www.oxygenboutique.com/SearchResults.aspx?') \
-                        #         and not url.startswith('https://www.oxygenboutique.com/SearchResults.aspx?') \
-                        #         and not url.startswith('http://www.oxygenboutique.com/GetImage/') \
-                        #         and not url.startswith('https://www.oxygenboutique.com/GetImage/'):
                         new_request = Request(new_url)
                         if url in self.urls_with_type.keys():
                             new_request.meta['lystType'] = self.urls_with_type[url]
@@ -96,7 +90,7 @@ class OxygenSpider(CrawlSpider):
     def parse_item(self, response):
         self.pq = pyquery.PyQuery(response.body)
 
-        root = self.pq('div #container')
+        root = self.pq('div#container')
 
         item = OxygendemoItem()
         # Field details
@@ -107,6 +101,8 @@ class OxygenSpider(CrawlSpider):
         #     - 'B' bags
         #     - 'J' jewelry
         #     - 'R' accessories
+
+        # there is no data for the ones that are only in the designers category
         if 'lystType' in response.request.meta.keys():
             item['type'] = response.request.meta['lystType']
 
@@ -120,29 +116,61 @@ class OxygenSpider(CrawlSpider):
         item['designer'] = designer
 
         # - code - unique identifier from a retailer perspective
+        full_name = root('h2').text().strip()
+        item['code'] = full_name
+
         # - name - short summary of the item
-        name = root('h2').text().strip()
+        name = full_name
         if name.startswith(designer + ' '):
             name = name[len(designer):].strip()
         item['name'] = name
+
         # - description - fuller description and details of the item
         # text of the element after the one with "Description" text
-
         dropdowns = [x for x in root('#accordion').children().items()]
         i = 0
         for i in range(len(dropdowns)):
             if dropdowns[i].text().strip() == 'Description':
                 break
 
-        item['description'] = dropdowns[i + 1].text().strip()
+        item['description'] = dropdowns[i + 1].text() \
+            .replace(u'\u00a0', ' ') \
+            .replace('\r', ' ') \
+            .replace('\n', ' ') \
+            .strip()
 
         # - raw_color - best guess of what colour the item is (can be blank if unidentifiable)
         # - image_urls - list of urls of large images representing the item
+        item['image_urls'] = [response.urljoin(href.extract()) for href in
+                              response.xpath('//div[@id="thumbnails"]//a/@href')]
+
         # - gbp_price - full (non-discounted) price of the item
+        price = root('.price').text().strip()
+        if price[0] == u'£':
+            price = price[1:].strip()
+        price = price.split(' ')
+        price = [float(x) for x in price]
+
+        item['gbp_price'] = price[0]
         # - sale_discount - percentage discount for sale items where applicable
+        if len(price) > 1:
+            item['sale_discount'] = 100 * ((price[0] - price[1]) / price[0])
+
         # - stock_status - dictionary of sizes to stock status
         # - 1 - out of stock
         # - 3 - in stock
+        stock = {}
+
+        sizes = root('select option')
+        for size in sizes:
+            if 'Please Select' in size.text:
+                continue
+            elif '- Sold Out' in size.text:
+                stock[size.text.replace('- Sold Out', '').strip()] = 1
+            else:
+                stock[size.text.strip()] = 3
+        item['stock_status'] = stock
+
         # - source_url - url of product page
         item['source_url'] = response.request.url
 
