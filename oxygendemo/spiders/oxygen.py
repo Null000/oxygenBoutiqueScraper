432497d6d9fff4b6a8638406541b62f237c6869f
Damjan Košir <damjan.kosir@gmail.com>
2015-08-13 23:44:03 +1200

    all done using rules

--- oxygendemo/spiders/oxygen.py
+++ oxygendemo/spiders/oxygen.py
@@ -1,91 +1,37 @@
 # coding=utf-8
-import urllib
-from scrapy import Request
-from scrapy.contrib.spiders import CrawlSpider
+from scrapy.spiders import CrawlSpider
 from oxygendemo.items import OxygendemoItem
+from scrapy.spiders import Rule
+from scrapy.linkextractors import LinkExtractor
 import pyquery
 
+
+def add_sale(url_array):
+    return url_array + [url + '&S=1' for url in url_array]
 
 class OxygenSpider(CrawlSpider):
     name = "oxygenboutique.com"
     allowed_domains = ["oxygenboutique.com"]
 
-    # ?ViewAll=1 is added to show all items on one page
-    clothes_url = 'http://www.oxygenboutique.com/clothing.aspx?ViewAll=1'
-    shoes_url = 'http://www.oxygenboutique.com/Shoes-All.aspx?ViewAll=1'
+    # ViewAll=1 is added to show all items on one page
+    # S=1 is for sale items
+    clothes_urls = add_sale(['http://www.oxygenboutique.com/clothing.aspx?ViewAll=1'])
+    shoes_urls = add_sale(['http://www.oxygenboutique.com/Shoes-All.aspx?ViewAll=1'])
+    jewelry_urls = add_sale(['http://www.oxygenboutique.com/earrings.aspx?ViewAll=1',
+                             'http://www.oxygenboutique.com/ring.aspx?ViewAll=1',
+                             'http://www.oxygenboutique.com/necklace.aspx?ViewAll=1'])
+    accessories_urls = add_sale(['http://www.oxygenboutique.com/hats.aspx?ViewAll=1',
+                                 'http://www.oxygenboutique.com/iphone-cases.aspx?ViewAll=1',
+                                 'http://www.oxygenboutique.com/HOMEWEAR.aspx?ViewAll=1',
+                                 'http://www.oxygenboutique.com/Tattoos.aspx?ViewAll=1',
+                                 'http://www.oxygenboutique.com/Crystal-Tattoos.aspx?ViewAll=1'])
 
-    jewelry_urls = ['http://www.oxygenboutique.com/earrings.aspx?ViewAll=1',
-                    'http://www.oxygenboutique.com/ring.aspx?ViewAll=1',
-                    'http://www.oxygenboutique.com/necklace.aspx?ViewAll=1']
-    accessories_url = 'http://www.oxygenboutique.com/accessories-all.aspx?ViewAll=1'
+    start_urls = clothes_urls + shoes_urls + accessories_urls
 
-    start_urls = [clothes_url, shoes_url] + jewelry_urls
-
-    jewelry_urls_remaining = set(jewelry_urls)
-    urls_before_designers_remaining = set(start_urls + [accessories_url])
-
-    visited_urls = set()
-
-    urls_with_type = {
-        clothes_url: 'A',
-        shoes_url: 'S',
-        accessories_url: 'R'
-    }
-    for url in jewelry_urls:
-        urls_with_type[url] = 'J'
-
-    def parse(self, response):
-        url = response.url
-        url = urllib.unquote(url)
-
-        self.visited_urls.add(url)
-        if url in self.jewelry_urls_remaining:
-            self.jewelry_urls_remaining.remove(url)
-        if url in self.urls_before_designers_remaining:
-            self.urls_before_designers_remaining.remove(url)
-
-        # if we can't use it, we ended up somewhere strange
-        if self.can_use_xpath(response):
-            self.logger.info('checking: %s', url)
-
-            if self.is_product_page(response):
-                yield self.parse_item(response)
-            else:
-                for href in response.xpath('//div[@id="container"]//div[@class="DataContainer"]//a/@href'):
-                    new_url = response.urljoin(href.extract())
-                    new_url = urllib.unquote(new_url)
-
-                    if new_url not in self.visited_urls:
-                        if url in self.jewelry_urls:
-                            self.jewelry_urls_remaining.add(new_url)
-                        self.urls_before_designers_remaining.add(new_url)
-
-                        new_request = Request(new_url)
-                        if url in self.urls_with_type.keys():
-                            new_request.meta['lystType'] = self.urls_with_type[url]
-                        yield new_request
-            if len(self.jewelry_urls_remaining) == 0:
-                # all jewelery done, adding the rest of accessories
-                yield Request(self.accessories_url)
-            if len(self.urls_before_designers_remaining) == 0:
-                # all items by type done, adding designers
-                for href in response.xpath('//ul[@id="ctl00_ulDesigners"]//a/@href'):
-                    new_url = response.urljoin(href.extract())
-                    new_url = urllib.unquote(new_url) + '?ViewAll=1'
-                    yield Request(new_url)
-        else:
-            # log these so you can figure out what not to follow
-            self.logger.info('not checking: %s', response.url)
-
-    def is_product_page(self, response):
-        if len(response.xpath('//div[@id="product-images"]')) > 0:
-            self.logger.info('found product page: %s', response.url)
-            return True
-        return False
-
-    @staticmethod
-    def can_use_xpath(response):
-        return getattr(response, 'xpath', None) is not None
+    rules = [
+        Rule(LinkExtractor(restrict_xpaths='//div[@id="container"]//div[@class="DataContainer"]'),
+             callback='parse_item')
+    ]
 
     def parse_item(self, response):
         self.pq = pyquery.PyQuery(response.body)
@@ -103,8 +49,16 @@ class OxygenSpider(CrawlSpider):
         #     - 'R' accessories
 
         # there is no data for the ones that are only in the designers category
-        if 'lystType' in response.request.meta.keys():
-            item['type'] = response.request.meta['lystType']
+        referer = response.request.headers.get('Referer')
+
+        item_type = 'A'
+        if referer in self.shoes_urls:
+            item_type = 'S'
+        elif referer in self.jewelry_urls:
+            item_type = 'J'
+        elif referer in self.accessories_urls:
+            item_type = 'R'
+        item['type'] = item_type
 
         # - gender, one of:
         # - 'F' female
